c8391e2d86bb2772be8e52e37736823ab71d1cd1
81andie <133395577+81andie@users.noreply.github.com>
2024-11-26 22:32:08 +0100

    Haciendo de nuevo la seccion de Magos

--- src/app/potter/potter.module.ts
+++ src/app/potter/potter.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PotterRoutingModule } from './potter-routing.module';
-import { MagosPageComponent } from './pages/magos-page/magos-page.component';
+
 import { EstudiantesHogwartsPageComponent } from './pages/estudiantes-hogwarts-page/estudiantes-hogwarts-page.component';
 import { HogwartsProfesPageComponent } from './pages/hogwarts-profes-page/hogwarts-profes-page.component';
 import { HechizosPageComponent } from './pages/hechizos-page/hechizos-page.component';
@@ -11,6 +11,7 @@ import { InicioComponent } from './pages/inicio-page/inicio.component';
 import { IniSection1Component } from './pages/ini-section1/ini-section1.component';
 import { SectionIni2Component } from './pages/section-ini2/section-ini2.component';
 import { Section3IniComponent } from './pages/section3-ini/section3-ini.component';
+import { MagosPageComponent } from './pages/magos-page/magos-page.component';
 
 
 
