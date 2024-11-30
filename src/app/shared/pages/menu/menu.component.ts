665318610f29c004a6b6e1657dfb5ab46025379e
81andie <133395577+81andie@users.noreply.github.com>
2024-11-30 15:35:47 +0100

    Cambiando el translateObject por selectTranslateObject en el menu

--- src/app/shared/pages/menu/menu.component.ts
+++ src/app/shared/pages/menu/menu.component.ts
@@ -34,15 +34,15 @@ export class MenuComponent implements OnInit{
 }
 
 private updateMenuItems() {
-  const menuTranslations = this.translocoService.translateObject('menu');
-  this.menuItems = [
-    { label: menuTranslations.inicio, icon: 'pi pi-home', routerLink: ['/'] },
-    { label: menuTranslations.magos, icon: 'pi pi-star', routerLink: ['magos'] },
-    { label: menuTranslations.estudiantes, icon: 'pi pi-users', routerLink: ['estudiantes'] },
-    { label: menuTranslations.hogwartsProfesores, icon: 'pi pi-book', routerLink: ['profesores'] },
-    { label: menuTranslations.todosHechizos, icon: 'pi pi-tags', routerLink: ['hechizos'] },
-  ];
-}
+  this.translocoService.selectTranslateObject('menu').subscribe((menuTranslations) => {
+    this.menuItems = [
+      { label: menuTranslations.inicio, icon: 'pi pi-home', routerLink: ['/'] },
+      { label: menuTranslations.magos, icon: 'pi pi-star', routerLink: ['magos'] },
+      { label: menuTranslations.estudiantes, icon: 'pi pi-users', routerLink: ['estudiantes'] },
+      { label: menuTranslations.hogwartsProfesores, icon: 'pi pi-book', routerLink: ['profesores'] },
+      { label: menuTranslations.todosHechizos, icon: 'pi pi-tags', routerLink: ['hechizos'] },
+    ];
+  });
 
 }
-
+}
